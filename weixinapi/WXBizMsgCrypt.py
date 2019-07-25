ccaada63c8e33bf254cfab91d4f34a9336b20d20
zong <zong-4@163.com>
2019-07-25 11:46:39 +0800

    fix weixin python sdk

--- weixinapi/WXBizMsgCrypt.py
+++ weixinapi/WXBizMsgCrypt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
-""" 对企业微信发送给企业后台的消息加解密示例代码.
+""" 对公众平台发送给公众账号的消息加解密示例代码.
 @copyright: Copyright (c) 1998-2014 Tencent Inc.
 
 """
@@ -10,11 +10,20 @@
 import base64
 import string
 import random
-import hashlib
+from hashlib import sha1
 import time
+import struct
 from Crypto.Cipher import AES
 import xml.etree.cElementTree as ET
+import sys
+import socket
 from . import ierror
+
+"""
+关于Crypto.Cipher模块，ImportError: No module named 'Crypto'解决方案
+请到官方网站 https://www.dlitz.net/software/pycrypto/ 下载pycrypto。
+下载后，按照README中的“Installation”小节的提示进行pycrypto安装。
+"""
 
 
 class FormatException(Exception):
@@ -26,17 +35,8 @@ def throw_exception(message, exception_class=FormatException):
     raise exception_class(message)
 
 
-def generateNonce(digits=16):
-    """ 随机生成16位字符串
-    @return: 16位字符串
-    """
-    rule = string.ascii_lowercase + string.digits
-    str = random.sample(rule, digits)
-    return "".join(str)
-
-
 class SHA1:
-    """计算企业微信的消息签名接口"""
+    """计算公众平台的消息签名接口"""
 
     def getSHA1(self, token, timestamp, nonce, encrypt):
         """用SHA1算法生成安全签名
@@ -49,7 +49,7 @@ class SHA1:
         try:
             sortlist = [token, timestamp, nonce, encrypt]
             sortlist.sort()
-            sha = hashlib.sha1()
+            sha = sha1()
             sha.update("".join(sortlist).encode('utf-8'))
             return ierror.WXBizMsgCrypt_OK, sha.hexdigest()
         except Exception as e:
@@ -61,12 +61,12 @@ class XMLParse:
     """提供提取消息格式中的密文及生成回复消息格式的接口"""
 
     # xml消息模板
-    AES_TEXT_RESPONSE_TEMPLATE = '<xml>' + \
-                                 '<Encrypt><![CDATA[%(msg_encrypt)s]]></Encrypt>' + \
-                                 '<MsgSignature><![CDATA[%(msg_signaturet)s]]></MsgSignature>' + \
-                                 '<TimeStamp>%(timestamp)s</TimeStamp>' + \
-                                 '<Nonce><![CDATA[%(nonce)s]]></Nonce>' + \
-                                 '</xml>'
+    AES_TEXT_RESPONSE_TEMPLATE = """<xml>
+<Encrypt><![CDATA[%(msg_encrypt)s]]></Encrypt>
+<MsgSignature><![CDATA[%(msg_signaturet)s]]></MsgSignature>
+<TimeStamp>%(timestamp)s</TimeStamp>
+<Nonce><![CDATA[%(nonce)s]]></Nonce>
+</xml>"""
 
     def extract(self, xmltext):
         """提取出xml数据包中的加密消息
@@ -76,7 +76,8 @@ class XMLParse:
         try:
             xml_tree = ET.fromstring(xmltext)
             encrypt = xml_tree.find("Encrypt")
-            return ierror.WXBizMsgCrypt_OK, encrypt.text
+            touser_name = xml_tree.find("ToUserName")
+            return ierror.WXBizMsgCrypt_OK, encrypt.text, touser_name.text
         except Exception as e:
             print(e)
             return ierror.WXBizMsgCrypt_ParseXml_Error, None, None
@@ -99,111 +100,6 @@ class XMLParse:
         return resp_xml
 
 
-class ResponseMessage():
-    # python dict 转换成特定格式的xml,下面是一些模板
-    """
-        text_response = {
-            'to_user':'',
-            'from_user':'',
-            'timestamp':'',
-            'type':'text',
-            'content':'',
-        }
-        voice_response= {
-            'to_user':'',
-            'from_user':'',
-            'timestamp':'',
-            'type':'voice',
-            'media_id':''
-        }
-        image_response= {
-            'to_user':'',
-            'from_user':'',
-            'timestamp':'',
-            'type':'image',
-            'data':[
-                {'media_id':''}
-            ]
-        }
-        video_response= {
-            'to_user':'',
-            'from_user':'',
-            'timestamp':'',
-            'type':'video',
-            'media_id':'',
-            'title':'',
-            'description':'',
-        }
-        article_response= {
-            'to_user':'',
-            'from_user':'',
-            'timestamp':'',
-            'type':'news',
-            'data':[
-                {'title':'',
-                 'description':'',
-                 'pic_url':'',
-                 'url':'',
-                }
-            ]
-        }
-
-    """
-    BASIC_RESPONSE_FIELDS = '<ToUserName><![CDATA[%(to_user)s]]></ToUserName>' + \
-                            '<FromUserName><![CDATA[%(from_user)s]]></FromUserName>' + \
-                            '<CreateTime>%(timestamp)s</CreateTime>' + \
-                            '<MsgType><![CDATA[%(type)s]]></MsgType>'
-
-    TEXT_RESPONSE_FIELD = "<Content><![CDATA[%(content)s]]></Content>"
-    VOICE_RESPONSE_FIELD = "<Voice><![CDATA[%(media_id)s]]></Voice>"
-    IMAGE_RESPONSE_FIELD = "<MediaId><![CDATA[%(media_id)s]]></MediaId>"
-    VIDEO_RESPONSE_FIELD = '<Video>' + \
-                           '<MediaId><![CDATA[%(media_id)s]]></MediaId>' + \
-                           '<Title><![CDATA[%(title)s]]></Title>' + \
-                           '<Description><![CDATA[%(description)s]]></Description>' + \
-                           '</Video>'
-    ARTICLE_RESPONSE_FIELD = '<items>' + \
-                             '<Title><![CDATA[%(title)s]]></Title>' + \
-                             '<Description><![CDATA[%(description)s]]></Description>' + \
-                             '<PicUrl><![CDATA[%(pic_url)s]]></PicUrl>' + \
-                             '<Url><![CDATA[%(url)s]]></Url>' + \
-                             '</items>'
-
-    def __init__(self, data_dict):
-
-        if 'timestamp' not in data_dict:
-            data_dict['timestamp'] = str(int(time.time()))
-        self.data = data_dict
-
-    @property
-    def xml(self):
-        basic = self.BASIC_RESPONSE_FIELDS % self.data
-        # text message
-        if self.data['type'] == 'text':
-            return '<xml>' + basic + self.TEXT_RESPONSE_FIELD % self.data + '</xml>'
-        # image message
-        elif self.data['type'] == 'image':
-            tmp = ''
-            for d in self.data['data']:
-                tmp = tmp + self.IMAGE_RESPONSE_FIELD % d
-            return '<xml>' + basic + '<Image>' + tmp + '</Image></xml>'
-        # voice message
-        elif self.data['type'] == 'voice':
-            return '<xml>' + basic + self.VOICE_RESPONSE_FIELD % self.data + '</xml>'
-        # video message
-        elif self.data['type'] == 'video':
-            return '<xml>' + basic + self.VIDEO_RESPONSE_FIELD % self.data + '</xml>'
-        # news message
-        elif self.data['type'] == 'news':
-            tmp = ''
-            for d in self.data['data']:
-                tmp = tmp + self.ARTICLE_RESPONSE_FIELD % d
-            count = "<ArticleCount>" + str(len(self.data['data'])) + "</ArticleCount>"
-            return '<xml>' + basic + count + '<Articles>' + tmp + '</Articles></xml>'
-        else:
-            return None
-
-
 class PKCS7Encoder():
     """提供基于PKCS7算法的加解密接口"""
 
@@ -221,8 +117,6 @@ class PKCS7Encoder():
             amount_to_pad = self.block_size
         # 获得补位所用的字符
         pad = chr(amount_to_pad)
-        if type(text) == bytes:
-            return text + amount_to_pad * amount_to_pad.to_bytes(1, 'big')
         return text + pad * amount_to_pad
 
     def decode(self, decrypted):
@@ -230,86 +124,92 @@ class PKCS7Encoder():
         @param decrypted: 解密后的明文
         @return: 删除补位字符后的明文
         """
-        pad = decrypted[-1]
+        pad = ord(decrypted[-1])
         if pad < 1 or pad > 32:
             pad = 0
         return decrypted[:-pad]
 
 
-class Prpcrypt(object):
+class Prpcrypt:
     """提供接收和推送给企业微信消息的加解密接口"""
 
     def __init__(self, key):
-
-        # self.key = base64.b64decode(key+"=")
-        self.key = key
-        # 设置加解密模式为AES的CBC模式
-        self.mode = AES.MODE_CBC
+        self.key = key  # AES算法的密钥，长度为32字节
+        self.mode = AES.MODE_CBC  # 设置加解密模式为AES的CBC模式
+        self.iv = key[:16]  # IV初始向量大小为16字节，取AESKey前16字节
 
     def encrypt(self, text, receiveid):
-        """对明文进行加密
-        @param text: 需要加密的明文
-        @return: 加密得到的字符串
         """
-        # 16位随机字符串添加到明文开头
-
-        text_bytes = text.encode('utf8')
-        text = generateNonce().encode('utf8') + int.to_bytes(len(text_bytes), 4,
-                                                             byteorder='big') + text_bytes + receiveid.encode('utf8')
+        对明文进行加密
+        :param text: 需要加密的明文
+        :param receiveid:
+        :return: 加密等到的字符串
+        """
+        # 16位随机字符串添加到明文开头，拼接明文字符串
+        text = self.get_random_str() + struct.pack('!I', len(text)) + text.encode() + receiveid
         # 使用自定义的填充方式对明文进行补位填充
-        pkcs7 = PKCS7Encoder()
-        text = pkcs7.encode(text)
+        text = PKCS7Encoder.encode(text)
         # 加密
-        cryptor = AES.new(self.key, self.mode, self.key[:16])
+        cryptor = AES.new(self.key, self.mode, self.iv)
         try:
             ciphertext = cryptor.encrypt(text)
             # 使用BASE64对加密后的字符串进行编码
-            return ierror.WXBizMsgCrypt_OK, base64.b64encode(ciphertext).decode('utf8')
+            return ierror.WXBizMsgCrypt_OK, base64.b64encode(ciphertext)
         except Exception as e:
             print(e)
             return ierror.WXBizMsgCrypt_EncryptAES_Error, None
 
     def decrypt(self, text, receiveid):
-        """对解密后的明文进行补位删除
-        @param text: 密文
-        @return: 删除填充补位后的明文
+        """
+        对解密后的明文进行补位删除
+        :param text: 密文
+        :param receiveid:
+        :return: 删除填充补位后的明文
         """
         try:
             cryptor = AES.new(self.key, self.mode, self.key[:16])
             # 使用BASE64对密文进行解码，然后AES-CBC解密
             plain_text = cryptor.decrypt(base64.b64decode(text))
-
         except Exception as e:
             print(e)
             return ierror.WXBizMsgCrypt_DecryptAES_Error, None
         try:
-            # pad = plain_text[-1]
-            # 去掉补位字符串
-            pkcs7 = PKCS7Encoder()
-            plain_text = pkcs7.decode(plain_text)
-
-            xml_len = int.from_bytes(plain_text[16:20], byteorder='big')
-            xml_content = plain_text[20: 20 + xml_len].decode('utf-8')
-            from_receiveid = plain_text[20 + xml_len:].decode('utf-8')
+            val = plain_text[-1]
+            content = plain_text[16:-val]
+            xml_len = socket.ntohl(struct.unpack("I", content[: 4])[0])
+            xml_content = content[4: xml_len + 4]
+            from_receiveid = content[xml_len + 4:]
         except Exception as e:
             print(e)
             return ierror.WXBizMsgCrypt_IllegalBuffer, None
-        if from_receiveid != receiveid:
+        if from_receiveid.decode() != receiveid:
             return ierror.WXBizMsgCrypt_ValidateCorpid_Error, None
         return 0, xml_content
+
+    def get_random_str(self):
+        """
+        随机生成16位字符串
+        :return: 16位字符串
+        """
+        rule = string.ascii_letters + string.digits
+        str = random.sample(rule, 16)
+        return ''.join(str).encode()
 
 
 class WXBizMsgCrypt(object):
     # 构造函数
-    def __init__(self, sToken, sEncodingAESKey, sReceiveId):
+    # @param sToken: 公众平台上，开发者设置的Token
+    # @param sEncodingAESKey: 公众平台上，开发者设置的EncodingAESKey
+    # @param sCorpId: 企业号的CorpId
+    def __init__(self, sToken, sEncodingAESKey, sCorpId):
         try:
             self.key = base64.b64decode(sEncodingAESKey + "=")
             assert len(self.key) == 32
         except:
             throw_exception("[error]: EncodingAESKey unvalid !", FormatException)
-            # return ierror.WXBizMsgCrypt_IllegalAesKey,None
+            # return ierror.WXBizMsgCrypt_IllegalAesKey)
         self.m_sToken = sToken
-        self.m_sReceiveId = sReceiveId
+        self.m_sCorpid = sCorpId
 
         # 验证URL
         # @param sMsgSignature: 签名串，对应URL参数的msg_signature
@@ -324,22 +224,21 @@ class WXBizMsgCrypt(object):
         ret, signature = sha1.getSHA1(self.m_sToken, sTimeStamp, sNonce, sEchoStr)
         if ret != 0:
             return ret, None
-        if not signature == sMsgSignature:
+        if signature != sMsgSignature:
             return ierror.WXBizMsgCrypt_ValidateSignature_Error, None
-
         pc = Prpcrypt(self.key)
-        ret, sReplyEchoStr = pc.decrypt(sEchoStr, self.m_sReceiveId)
+        ret, sReplyEchoStr = pc.decrypt(sEchoStr, self.m_sCorpid)
         return ret, sReplyEchoStr
 
     def EncryptMsg(self, sReplyMsg, sNonce, timestamp=None):
-        # 将企业回复用户的消息加密打包
+        # 将公众号回复用户的消息加密打包
         # @param sReplyMsg: 企业号待回复用户的消息，xml格式的字符串
         # @param sTimeStamp: 时间戳，可以自己生成，也可以用URL参数的timestamp,如为None则自动用当前时间
         # @param sNonce: 随机串，可以自己生成，也可以用URL参数的nonce
         # sEncryptMsg: 加密后的可以直接回复用户的密文，包括msg_signature, timestamp, nonce, encrypt的xml格式的字符串,
         # return：成功0，sEncryptMsg,失败返回对应的错误码None
         pc = Prpcrypt(self.key)
-        ret, encrypt = pc.encrypt(sReplyMsg, self.m_sReceiveId)
+        ret, encrypt = pc.encrypt(sReplyMsg, self.m_sCorpid)
         if ret != 0:
             return ret, None
         if timestamp is None:
@@ -362,7 +261,7 @@ class WXBizMsgCrypt(object):
         # @return: 成功0，失败返回对应的错误码
         # 验证安全签名
         xmlParse = XMLParse()
-        ret, encrypt = xmlParse.extract(sPostData)
+        ret, encrypt, touser_name = xmlParse.extract(sPostData)
         if ret != 0:
             return ret, None
         sha1 = SHA1()
@@ -372,5 +271,5 @@ class WXBizMsgCrypt(object):
         if not signature == sMsgSignature:
             return ierror.WXBizMsgCrypt_ValidateSignature_Error, None
         pc = Prpcrypt(self.key)
-        ret, xml_content = pc.decrypt(encrypt, self.m_sReceiveId)
+        ret, xml_content = pc.decrypt(encrypt, self.m_sCorpid)
         return ret, xml_content
--- weixinapi/WXCallback.py
+++ weixinapi/WXCallback.py
@@ -1,5 +1,6 @@
 from .WXBizMsgCrypt import WXBizMsgCrypt
 from bxework.config import config
+from urllib import parse
 
 def __get_wxcrypt():
     conf = config()
@@ -9,7 +10,8 @@ def __get_wxcrypt():
 
 def verfiy_echo(request_args):
     wxcrypt = __get_wxcrypt()
-    ret, sReplyEchoStr = wxcrypt.VerifyURL(request_args['msg_signature'], request_args['timestamp'], request_args['nonce'], request_args['echostr'])
+    sEchoStr = parse.unquote(request_args['echostr'], encoding='utf-8', errors='replace')
+    ret, sReplyEchoStr = wxcrypt.VerifyURL(request_args['msg_signature'], request_args['timestamp'], request_args['nonce'], sEchoStr)
     if ret != 0:
         return 'error: verify fail, errcode: {}'.format(ret)
     return sReplyEchoStr
